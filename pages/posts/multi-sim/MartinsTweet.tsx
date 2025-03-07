29807a74faad282086e880029d736a1d3c448d13
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-03-07 10:25:29 +0100

    publish multi-sim post

---
+++ pages/posts/multi-sim/MartinsTweet.tsx
@@ -0,0 +1,21 @@
+export default function MartinsTweet() {
+  return (
+    <div style={{ margin: "0 auto", width: 550 }}>
+      <blockquote className="twitter-tweet">
+        <p lang="en" dir="ltr">
+          Are you confident that if your computer sent your hardware wallet a
+          different transaction from what is shown in the dApp/wallet, you would
+          catch it?
+          <br />
+          <br />I don&#39;t know the survey result yet, but I know we will need
+          to improve it dramatically.
+        </p>
+        &mdash; koeppelmann.eth 🦉💳 (@koeppelmann){" "}
+        <a href="https://twitter.com/koeppelmann/status/1897282469189312937?ref_src=twsrc%5Etfw">
+          March 5, 2025
+        </a>
+      </blockquote>
+      <script async src="https://platform.twitter.com/widgets.js"></script>
+    </div>
+  )
+}
